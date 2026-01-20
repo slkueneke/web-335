6d6cec57c80e7608a1505e4942582613f4600d7f
Shannon Kueneke <slkueneke@gmail.com>
2026-01-19 16:13:48 -0800

    python exercise

---
+++ week-2/exercise_1-1.py
@@ -0,0 +1,11 @@
+""" 
+    Title: exercise_1-1.py
+    Author: Shannon Kueneke
+    Date: Jan 19 2026
+    Description: Print a simple quote with author.
+"""
+
+quote = "Find what you love and let it kill you."
+author = "Charles Bukowski"
+print('"' + quote + '" ~ ' + author)
+
